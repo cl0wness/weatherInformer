e216b561668d84c8487cccb93f5217b88bbd5e45
Polina <clown.es@yandex.ru>
2025-06-07 23:58:44 +0300

    реализован репозиторий

---
+++ data/weather/api/src/main/java/com/app/data/weather/api/repository/WeatherRepository.kt
@@ -0,0 +1,17 @@
+package com.app.data.weather.api.repository
+
+import com.app.data.weather.api.core.Resource
+import com.app.data.weather.api.model.CurrentWeatherModel
+import com.app.data.weather.api.model.DailyForecast
+
+/**
+ * Репозиторий для получения данных о погоде.
+ */
+interface WeatherRepository {
+
+    /** Получение текущей погоды по координатам (широта, долгота). */
+    suspend fun fetchCurrentWeather(lat: Double, lon: Double): Resource<CurrentWeatherModel, String>
+
+    /** Получение прогноза погоды на неделю по координатам (широта, долгота). */
+    suspend fun fetchWeeklyWeather(lat: Double, lon: Double) : Resource<List<DailyForecast>, String>
+}
---
+++ data/weather/impl/src/main/java/com/app/data/weather/impl/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.app.data.weather.impl.repository
+
+import com.app.data.weather.api.core.Resource
+import com.app.data.weather.api.model.CurrentWeatherModel
+import com.app.data.weather.api.model.DailyForecast
+import com.app.data.weather.api.repository.WeatherRepository
+import com.app.data.weather.impl.remote.dataSource.WeatherDataSource
+import com.app.data.weather.impl.remote.model.toDomain
+import toDomain
+
+class WeatherRepositoryImpl (
+    private val dataSource: WeatherDataSource
+) : WeatherRepository {
+    override suspend fun fetchCurrentWeather(
+        lat: Double,
+        lon: Double
+    ): Resource<CurrentWeatherModel, String> {
+        return dataSource.fetchCurrentWeather(lat = lat, lon = lon)
+            .mapData { it.data.first().toDomain() }
+    }
+
+    override suspend fun fetchWeeklyWeather(
+        lat: Double,
+        lon: Double
+    ): Resource<List<DailyForecast>, String> {
+        return dataSource.fetchWeekWeather(lat = lat, lon = lon).mapData { response ->
+            response.data.map { it.toDomain() }
+        }
+    }
+}
